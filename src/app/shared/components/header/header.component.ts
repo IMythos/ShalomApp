d80e0e20066fa1278e7d3c11116b31fdb43a684d
mythos <developerwebmythos@gmail.com>
2025-11-17 01:10:22 -0500

    EF003 - Header design component successfully. Responsive design, no navs.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+import { LucideAngularModule, House, Search, MapPin, ClipboardList, SquareMenu, CircleX, UserRoundPlus } from "lucide-angular";
 
 @Component({
   selector: 'client-header',
+  standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: 'header.component.html'
 })
 
 export class HeaderComponent {
 
+  // Icons
+
+  public readonly HomeIcon = House;
+  public readonly SearchIcon = Search;
+  public readonly MapPinIcon = MapPin;
+  public readonly ClipboardListIcon = ClipboardList;
+  public readonly SquareMenuIcon = SquareMenu;
+  public readonly CircleXIcon = CircleX;
+  public readonly UserRoundPlusIcon = UserRoundPlus;
+
+  // Signals
+
+  public isOpen = signal(false);
+
+  // Methods
+
+  public toggleMenu() : void {
+    this.isOpen.update(state => !state);
+  }
 }
