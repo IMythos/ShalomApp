8ddae4afbda33618dea970e5a270ef94bf978793
mythos <developerwebmythos@gmail.com>
2025-11-17 13:28:01 -0500

    EF005 - Footer added in client home page. Responsive.

--- src/app/features/client/pages/home/home.component.ts
+++ src/app/features/client/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { HeaderComponent } from "../../../../shared/components/header/header.component";
 import { BannerComponent } from "./banner/banner.component";
 import { LucideAngularModule, ArrowRight } from "lucide-angular";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'client-home',
-  imports: [HeaderComponent, BannerComponent, LucideAngularModule],
+  imports: [HeaderComponent, BannerComponent, LucideAngularModule, FooterComponent],
   styleUrl: 'home.component.css',
   templateUrl: 'home.component.html',
 })
